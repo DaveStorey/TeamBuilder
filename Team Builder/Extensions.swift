9891233b2373ac3c7713ec5bbc9bb815923c9bb4
Dave Storey <dstorey65@gmail.com>
2025-01-06 15:40:25 -0600

    Removing unneeded extension

--- Team Builder/Extensions.swift
+++ Team Builder/Extensions.swift
@@ -13,14 +13,3 @@ extension Optional where Wrapped == String {
         return self?.isEmpty ?? true
     }
 }
-
-extension RangeExpression where Bound: FixedWidthInteger {
-    func randomElements(_ n: Int) -> [Bound] {
-        precondition(n > 0)
-        switch self {
-        case let range as Range<Bound>: return (0..<n).map { _ in .random(in: range) }
-        case let range as ClosedRange<Bound>: return (0..<n).map { _ in .random(in: range) }
-        default: return []
-        }
-    }
-}
