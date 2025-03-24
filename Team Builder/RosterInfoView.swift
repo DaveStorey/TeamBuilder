47189259fbd4863b7c49c2c2efa3a9df71c97300
Dave Storey <dstorey65@gmail.com>
2025-03-24 18:43:44 -0500

    Added sorting option to roster stats view, created gitignore

--- Team Builder/RosterInfoView.swift
+++ Team Builder/RosterInfoView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct RosterInfoView: View {
+    private enum SortedBy: String {
+            case percent = "Percentage"
+            case wins = "Wins"
+            case losses = "Losses"
+            case name = "Name"
+            
+            var next: SortedBy {
+                switch self {
+                case .percent: return .wins
+                case .wins: return .losses
+                case .losses: return .name
+                case .name: return .percent
+                }
+            }
+        }
+    
     let players: [Player]
+    @State private var sortedBy: SortedBy = .percent
     
     var body: some View {
         List {
-            ForEach(players) { player in
+            ForEach(players.sorted(by: {
+                switch sortedBy {
+                case .percent:
+                    if $0.winningPercentage == $1.winningPercentage {
+                        return $0.wins > $1.wins
+                    } else {
+                        return $0.winningPercentage > $1.winningPercentage
+                    }
+                case .wins :
+                    return $0.wins > $1.wins
+                case .losses:
+                    return $0.losses > $1.losses
+                case .name:
+                    return $0.name < $1.name
+                }
+            })) { player in
                 Section(header: HStack {
                     Spacer()
                     Text(player.name).font(.headline)
@@ -36,6 +68,13 @@ struct RosterInfoView: View {
                     }
                 }
             }
+        }
+        .toolbar {
+            ToolbarItem(placement: .automatic, content: {
+                        Button("Sorted by \(sortedBy.rawValue)", action: {
+                            sortedBy = sortedBy.next
+                        })
+                    })
         }
     }
 }
